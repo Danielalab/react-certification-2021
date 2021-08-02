5127b907d3823f322446f1171d6718ff3538ba5e
Danielalab <olabolacrayola@hotmail.com>
2021-08-02 17:53:59 -0500

    feat(App): add routes

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import HomePage from '../../pages/Home';
+import VideoDetail from '../../pages/VideoDetail';
 import Header from '../Header';
 import Layout from '../Layout';
 
 function App() {
+  const [searchText, setSearchText] = useState('coldplay');
   return (
-    <Layout>
-      <Header />
-      <HomePage />
-    </Layout>
+    <Router>
+      <Layout>
+        <Header handleInputChange={setSearchText} />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage text={searchText} />
+          </Route>
+          <Route path="/:id">
+            <VideoDetail />
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
